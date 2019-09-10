2ee58ec9aa6d6952ace91b5ef4e74cbdb7aa124d
IlyaPridanov <pridanov.ilya@mail.ru>
2019-09-10 22:56:45 +0300

    правит баг с фоном на главной

--- source/js/script.js
+++ source/js/script.js
@@ -7,7 +7,9 @@ window.onload = function () {
   var btnSlider = document.querySelectorAll('.btn');
   var serviceBlock = document.querySelectorAll('.service-block');
   var tel = document.querySelector('#tel');
-  /*var upSliderLink = document.querySelectorAll('.up-slider__link');*/
+  var name = document.querySelector('#name');
+  var mail = document.querySelector('#mail');
+  var speach = document.querySelector('#speach');
 
   menuOpenBtn.addEventListener('click', function (e) {
     e.preventDefault();
@@ -25,7 +27,7 @@ window.onload = function () {
   }
 
   for (i = 0; i < serviceBlock.length; i++) {
-    serviceBlock[i].addEventListener('click', function (e) {
+    serviceBlock[i].addEventListener('click', function () {
       if (this.classList.contains('service-block--active')) {
         this.classList.remove('service-block--active');
       } else {
@@ -37,56 +39,6 @@ window.onload = function () {
     });
   }
 
-  /*Добавляет маску*/
-
-
-  var maskOptions = {
-    mask: '+{7}(000)000-00-00'
-  };
-  var mask1 = IMask(tel, maskOptions);
-
-  /*var body = document.querySelector('body');
-  var openPopap = document.querySelector('.header__call-link');
-  var closePopap = document.querySelector('.popap__close');
-  var popap = document.querySelector('.popap');
-  var name = document.querySelector('#popap-name');
-  var tel = document.querySelector('#popap-tel');
-  var textbox = document.querySelector('#textbox');
-  var advantagesLink = document.querySelectorAll('.advantages__link');
-  var footerCaptionLink = document.querySelectorAll('.footer-center__caption-block');
-  var formTel = document.getElementById('form-tel');
-
-  openPopap.addEventListener('click', function (e) {
-    e.preventDefault();
-    body.style.overflow = 'hidden';
-    popap.classList.remove('hidden');
-    name.focus();
-  });
-
-  popap.addEventListener('click', function (event) {
-    if (event.target === this) {
-      popap.classList.add('hidden');
-      body.style.overflow = 'visible';
-      name.blur();
-    }
-  });
-
-  closePopap.addEventListener('click', function (e) {
-    e.preventDefault();
-    body.style.overflow = 'visible';
-    name.blur();
-    popap.classList.add('hidden');
-  });
-
-  window.addEventListener('keydown', function (e) {
-    if (e.keyCode === 27) {
-      e.preventDefault();
-      name.blur();
-      body.style.overflow = 'visible';
-      popap.classList.add('hidden');
-    }
-  });
-
   var getLocalSave = function (input) {
     input.addEventListener('change', function () {
       // eslint-disable-next-line no-invalid-this
@@ -96,30 +48,11 @@ window.onload = function () {
 
   getLocalSave(name);
   getLocalSave(tel);
-  getLocalSave(textbox);
+  getLocalSave(mail);
+  getLocalSave(speach);
 
-  for (var i = 0; i < advantagesLink.length; i++) {
-    advantagesLink[i].addEventListener('click', function (e) {
-      e.preventDefault();
-    });
-  }
-
-  for (var j = 0; j < footerCaptionLink.length; j++) {
-    footerCaptionLink[j].addEventListener('click', function (e) {
-      e.preventDefault();
-      var parent = this.parentNode;
-      var parentUse = parent.querySelector('use');
-      parent.classList.toggle('active');
-      if (parent.classList.contains('active')) {
-        parentUse.setAttributeNS('http://www.w3.org/1999/xlink', "href", "#icon-plus");
-      } else {
-        parentUse.setAttributeNS('http://www.w3.org/1999/xlink', "href", "#icon-minus");
-      }
-      var parentList = parent.querySelectorAll('ul');
-      for (var k = 0; k < parentList.length; k++) {
-        parentList[k].classList.toggle('footer-center__list--mobile-hidden');
-        parentList[k].classList.toggle('footer-center__list--mobile-visuallyhidden');
-      }
-    });
-  }*/
+  var maskOptions = {
+    mask: '+{7}(000)000-00-00'
+  };
+  var mask1 = IMask(tel, maskOptions);
 };
